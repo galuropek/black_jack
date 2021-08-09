b406148fbcab672f5cc3278287bd2bb9dd04a023
Ihar <galuropek@gmail.com>
2021-08-10 00:42:41 +0300

    updated base_user model

--- models/base_user.rb
+++ models/base_user.rb
@@ -1,2 +1,46 @@
+require_relative 'bank'
+
 class BaseUser
+  attr_reader :name, :bank, :hand
+
+  def initialize(name)
+    @name = name
+  end
+
+  def init_bank(balance, bet)
+    self.bank = Bank.new(balance, bet)
+  end
+
+  def init_hand(first_card, second_card)
+    self.hand = [first_card, second_card]
+  end
+
+  def add_card_to_hand(card)
+    hand << card
+  end
+
+  def hand_sum
+    hand.map(&:numeric).sum
+  end
+
+  def show_hand
+    hand.each { |card| print "#{card} " }
+  end
+
+  def win
+    bank.win_bet
+  end
+
+  def lose
+    bank.lose_bet
+  end
+
+  def to_s
+    name
+  end
+
+  protected
+
+  attr_writer :name, :bank, :hand
+
 end

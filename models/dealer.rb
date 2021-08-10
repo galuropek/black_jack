4f1c8c11168e66899b86628195d5d3854e3705d4
Ihar <galuropek@gmail.com>
2021-08-10 18:54:13 +0300

    add msg attr and override hand_str method

--- models/dealer.rb
+++ models/dealer.rb
@@ -1,10 +1,17 @@
+# frozen_string_literal: true
+
 require_relative 'base_user'
 
 class Dealer < BaseUser
+  attr_accessor :msg
 
   EXPECTED_MAX_VALUE = 17
 
   def need_card?
     hand_sum < EXPECTED_MAX_VALUE
+  end
+
+  def hand_str
+    hand.map { |_| '*' }.join(' ')
   end
 end

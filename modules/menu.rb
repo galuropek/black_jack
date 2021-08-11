ef025d86252e2627b8a36e6e0a136c07028a2ec4
Ihar <galuropek@gmail.com>
2021-08-11 23:14:31 +0300

    transfer TABLE to menu

--- modules/menu.rb
+++ modules/menu.rb
@@ -1,26 +1,58 @@
 # frozen_string_literal: true
 
 module Menu
-  INCORRECT_INPUT = 'Incorect input value! Use numeric only from list above!'
+  INCORRECT_INPUT = 'Incorrect input value! Use numeric only from list above!'
   YOUR_CHOICE = 'Enter your choice: '
+  TABLE = '
+******************
+*** Black Jack ***
+******************
+
+%<result>s
+
+Game: %<game>s                     Round: %<round>s
+------------------------------------
+		Dealer:
+Bank: %<dealer_bank>s		%<bet>s
+Hand: %<dealer_hand>s
+%<dealer_message>s
+------------------------------------
+		%<user_name>s:
+Bank: %<user_bank>s		%<bet>s
+Hand: %<user_hand>s
+Your sum: %<user_sum>s
+------------------------------------
+'
 
   protected
 
-  def show_menu
-    main_menu.each_with_index { |action, index| puts "#{index}. #{action[:value]}." }
+  def show_menu(menu)
+    menu.each_with_index { |action, index| puts "#{index}. #{action[:value]}." }
   end
 
-  def main_menu
+  def play_menu
     [
       { value: 'Exit', action: :exit },
-      { value: 'One more card', action: :one_more_card },
-      { value: 'Pass', action: :pass }
+      { value: 'One more card', action: :one_more_card_user },
+      { value: 'Pass', action: :pass },
+      { value: 'Show cards', action: :end_game }
     ]
   end
 
-  def user_action
+  def end_game_menu
+    [
+      { value: 'Exit', action: :exit },
+      { value: 'Play again', action: :next_game }
+    ]
+  end
+
+  def user_choice(menu)
     print YOUR_CHOICE
     input_value = gets.chomp.to_i
-    main_menu[input_value.to_i][:action]
+    menu[input_value.to_i][:action]
+  rescue NoMethodError => e
+    puts "ERROR: #{e.message}"
+    puts INCORRECT_INPUT
+    retry
   end
 end

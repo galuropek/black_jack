2615fabcb61aec95cb447f0714da6a5b319f6b0b
Ihar <galuropek@gmail.com>
2021-08-11 23:12:27 +0300

    add alternative? and alternative_mode! methods

--- models/card.rb
+++ models/card.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 
 class Card
-  attr_reader :value, :suit, :numeric, :alternative_value
+  attr_reader :value, :suit, :numeric, :alternative_value, :mode
 
   def initialize(value, suit, numeric, alternative_value = nil)
     @alternative_value = alternative_value
     @numeric = numeric
     @value = value
     @suit = suit
+    @mode = :default
+  end
+
+  def alternative_mode!
+    return unless @alternative_value
+
+    @mode = :alternative
+    @numeric = @alternative_value
+  end
+
+  def alternative?
+    @alternative_value && @mode == :default
   end
 
   def to_s

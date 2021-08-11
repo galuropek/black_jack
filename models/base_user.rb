e4d60a9be01703a97b73babfff6c27c2af232e8f
Ihar <galuropek@gmail.com>
2021-08-11 23:11:52 +0300

    update hand_sum logic and add mew methods

--- models/base_user.rb
+++ models/base_user.rb
@@ -3,7 +3,10 @@
 require_relative 'bank'
 
 class BaseUser
+  attr_accessor :result
   attr_reader :name, :bank, :hand
+
+  EXPECTED_VALUE = 21
 
   def initialize(name)
     @name = name
@@ -21,12 +24,15 @@ class BaseUser
     hand << card
   end
 
-  def hand_sum
-    hand.map(&:numeric).sum
-  end
-
   def show_hand
     puts hand_str
+  end
+
+  def hand_sum
+    sum = hand.map(&:numeric).sum
+    return sum if sum <= EXPECTED_VALUE
+
+    include_alternative_values? ? activate_alternative_mode : sum
   end
 
   def hand_str
@@ -37,11 +43,17 @@ class BaseUser
     bank.win_bet
   end
 
+  alias return_bet win
+
   def lose
     bank.lose_bet
   end
 
   alias place_bet lose
+
+  def over?
+    hand_sum > EXPECTED_VALUE
+  end
 
   def to_s
     name
@@ -50,4 +62,17 @@ class BaseUser
   protected
 
   attr_writer :name, :bank, :hand
+
+  def activate_alternative_mode
+    hand.each do |card|
+      if card.alternative?
+        card.alternative_mode!
+        return hand_sum
+      end
+    end
+  end
+
+  def include_alternative_values?
+    !!hand.find(&:alternative?)
+  end
 end
